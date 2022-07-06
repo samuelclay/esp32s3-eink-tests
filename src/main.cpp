ef7b2d0ded0272fa7981a52c553b62f74cf3354a
Samuel Clay <samuel@ofbrooklyn.com>
2022-07-06 12:39:07 -0400

    Re-arranging before fixing bluetooth and display.

--- src/main.cpp
+++ src/main.cpp
@@ -11,26 +11,26 @@
 
 #define PIN_NEOPIXEL 48
 Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+#define EPD_DEBUG   1
 
 #define EPD_CS      15
 #define EPD_DC      16
 #define SRAM_CS     3
 #define SD_CS       46
-#define EPD_RESET   -1 // can set to -1 and share with microcontroller Reset!
-#define EPD_BUSY    -1 // can set to -1 to not use a pin (will wait a fixed delay)
+#define EPD_RESET   4 // can set to -1 and share with microcontroller Reset!
+#define EPD_BUSY    17 // can set to -1 to not use a pin (will wait a fixed delay)
 
 #define EPD_WIDTH   200
 #define EPD_HEIGHT  200
 #define HSPI_MISO 12
 #define HSPI_MOSI 13
 #define HSPI_SCLK 14
-#define HSPI_CS   EPD_CS
+#define HSPI_CS   15
 SPIClass * hspi = new SPIClass(HSPI);
-SPIClass SPI3(HSPI);
+// SPIClass SPI3(FSPI);
 // Adafruit_SSD1681 display(200, 200, HSPI_MOSI, HSPI_SCLK, EPD_DC,
 //                    EPD_RESET, EPD_CS, SRAM_CS, HSPI_MISO,
 //                    EPD_BUSY);
-Adafruit_SSD1681 display = Adafruit_SSD1681(EPD_WIDTH, EPD_HEIGHT, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, &SPI3);
 
 // Bluetooth Low Energy
 // Turn Touch Remote Control
@@ -69,17 +69,17 @@ bool connectToRemoteDevice(BLEAddress pAddress)
     Serial.println(" ---> Failed to find button status service.");
     return false;
   }
-  BLERemoteService *pBatteryService = pClient->getService(ttBatteryStatusServiceUuid);
-  if (!pBatteryService) {
-    Serial.println(" ---> Failed to find button status service.");
-    return false;
-  }
+  // BLERemoteService *pBatteryService = pClient->getService(ttBatteryStatusServiceUuid);
+  // if (!pBatteryService) {
+  //   Serial.println(" ---> Failed to find button status service.");
+  //   return false;
+  // }
 
   buttonStatusCharacteristic = pButtonService->getCharacteristic(ttButtonStatusCharacteristicUuid);
-  deviceNicknameCharacteristic = pButtonService->getCharacteristic(ttDeviceNicknameCharacteristicUuid);
-  batteryLevelCharacteristic = pBatteryService->getCharacteristic(ttBatteryLevelCharacteristicUuid);
+  // deviceNicknameCharacteristic = pButtonService->getCharacteristic(ttDeviceNicknameCharacteristicUuid);
+  // batteryLevelCharacteristic = pBatteryService->getCharacteristic(ttBatteryLevelCharacteristicUuid);
 
-  if (!buttonStatusCharacteristic || !deviceNicknameCharacteristic || !batteryLevelCharacteristic) {
+  if (!buttonStatusCharacteristic) {//} || !deviceNicknameCharacteristic || !batteryLevelCharacteristic) {
     Serial.println(" ---> Failed to find characteristics.");
     return false;
   }
@@ -109,16 +109,19 @@ class AdvertisingCallback: public BLEAdvertisedDeviceCallbacks {
 };
 
 void setup() {
-  hspi->begin();
-  hspi->begin(HSPI_SCLK, HSPI_MISO, HSPI_MOSI, HSPI_CS); //SCLK, MISO, MOSI, SS
   pinMode(HSPI_CS, OUTPUT); //HSPI SS
+  hspi->begin(HSPI_SCLK, HSPI_MISO, HSPI_MOSI, HSPI_CS); //SCLK, MISO, MOSI, SS
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   pixels.setBrightness(20); // not so bright
 
-  Serial.begin(115200);
-  Serial.print("Hello! EPD Test");
+  pixels.fill(0xFF0000);
+  pixels.show();
 
+  Serial.begin(115200);
+  Serial.println("Hello! EPD Test");
+
+  Adafruit_SSD1681 display = Adafruit_SSD1681(EPD_WIDTH, EPD_HEIGHT, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, hspi);
   display.begin();
 #if defined(FLEXIBLE_213) || defined(FLEXIBLE_290)
   // The flexible displays have different buffers and invert settings!
@@ -143,18 +146,31 @@ void setup() {
   display.setTextColor(EPD_RED);
   display.print("--Eben Upton");
 
+  pixels.fill(0x00FF00);
+  pixels.show();
+
   display.display();
 
+  pixels.fill(0x0000FF);
+  pixels.show();
+
+  Serial.println(" ---> Initializing BLE...");
   BLEDevice::init("");
   BLEScan *pScanner = BLEDevice::getScan();
   pScanner->setAdvertisedDeviceCallbacks(new AdvertisingCallback());
   pScanner->setActiveScan(true);
-  pScanner->start(30, false);
+  pScanner->start(4, false);
+
+  pixels.fill(0x00FF00);
+  pixels.show();
 }
 
 void loop() {
-  Serial.println("Hello!");
-  // set color to red
+  Serial.print("Loop: needsConnect=");
+  Serial.print(needsConnect);
+  Serial.print(" isConnected=");
+  Serial.println(isConnected);
+
   pixels.fill(0xFF00FF);
   pixels.show();
   delay(500); // wait half a second
