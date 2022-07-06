a7f43de80786fe70bf7c51f23a0590bc6f37fe0f
Samuel Clay <samuel@ofbrooklyn.com>
2022-07-06 11:38:14 -0400

    Adding BLE for turn touch. Also fixing serial monitor.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include "Adafruit_EPD.h"
 #include <SPI.h>
+#include "BLEDevice.h"
+
+
+// SSD1681 E-Ink Display
+// 1.54" 200x200 Monochrome E-Ink
 
 #define PIN_NEOPIXEL 48
 Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
@@ -14,6 +19,8 @@ Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 #define EPD_RESET   -1 // can set to -1 and share with microcontroller Reset!
 #define EPD_BUSY    -1 // can set to -1 to not use a pin (will wait a fixed delay)
 
+#define EPD_WIDTH   200
+#define EPD_HEIGHT  200
 #define HSPI_MISO 12
 #define HSPI_MOSI 13
 #define HSPI_SCLK 14
@@ -23,7 +30,83 @@ SPIClass SPI3(HSPI);
 // Adafruit_SSD1681 display(200, 200, HSPI_MOSI, HSPI_SCLK, EPD_DC,
 //                    EPD_RESET, EPD_CS, SRAM_CS, HSPI_MISO,
 //                    EPD_BUSY);
-Adafruit_SSD1681 display = Adafruit_SSD1681(200, 200, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, &SPI3);
+Adafruit_SSD1681 display = Adafruit_SSD1681(EPD_WIDTH, EPD_HEIGHT, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, &SPI3);
+
+// Bluetooth Low Energy
+// Turn Touch Remote Control
+
+#define TT_BLE_NAME "Turn Touch Remote"
+static BLEUUID ttButtonStatusServiceUuid("99c31523-dc4f-41b1-bb04-4e4deb81fadd");
+static BLEUUID ttButtonStatusCharacteristicUuid("99c31525-dc4f-41b1-bb04-4e4deb81fadd");
+static BLEUUID ttDeviceNicknameCharacteristicUuid("99c31526-dc4f-41b1-bb04-4e4deb81fadd");
+static BLEUUID ttBatteryStatusServiceUuid("180F");
+static BLEUUID ttBatteryLevelCharacteristicUuid("99c31523-dc4f-41b1-bb04-4e4deb81fadd");
+static bool needsConnect = false;
+static bool isConnected = false;
+static BLEAddress *pRemoteAddress;
+static BLERemoteCharacteristic *buttonStatusCharacteristic;
+static BLERemoteCharacteristic *deviceNicknameCharacteristic;
+static BLERemoteCharacteristic *batteryLevelCharacteristic;
+const uint8_t notificationOn[] = {0x1, 0x0};
+const uint8_t notificationOff[] = {0x0, 0x0};
+char *buttonStatus;
+char *deviceNickname;
+float batteryLevel;
+bool newButtonStatus = false;
+bool newDeviceNickname = false;
+bool newBatteryLevel = false;
+
+static void handleButtonStatusNotification(BLERemoteCharacteristic *pCharacteristic, uint8_t *pData, size_t length, bool isNotify);
+
+bool connectToRemoteDevice(BLEAddress pAddress)
+{
+  BLEClient *pClient = BLEDevice::createClient();
+  pClient->connect(pAddress);
+  Serial.println(" ---> Connected to remote device.");
+
+  BLERemoteService *pButtonService = pClient->getService(ttButtonStatusServiceUuid);
+  if (!pButtonService) {
+    Serial.println(" ---> Failed to find button status service.");
+    return false;
+  }
+  BLERemoteService *pBatteryService = pClient->getService(ttBatteryStatusServiceUuid);
+  if (!pBatteryService) {
+    Serial.println(" ---> Failed to find button status service.");
+    return false;
+  }
+
+  buttonStatusCharacteristic = pButtonService->getCharacteristic(ttButtonStatusCharacteristicUuid);
+  deviceNicknameCharacteristic = pButtonService->getCharacteristic(ttDeviceNicknameCharacteristicUuid);
+  batteryLevelCharacteristic = pBatteryService->getCharacteristic(ttBatteryLevelCharacteristicUuid);
+
+  if (!buttonStatusCharacteristic || !deviceNicknameCharacteristic || !batteryLevelCharacteristic) {
+    Serial.println(" ---> Failed to find characteristics.");
+    return false;
+  }
+
+  buttonStatusCharacteristic->registerForNotify(handleButtonStatusNotification);
+
+  return true;
+}
+
+static void handleButtonStatusNotification(BLERemoteCharacteristic *pCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
+  if (isNotify) {
+    Serial.println(" ---> Button status notification received.");
+    buttonStatus = (char *)pData;
+    newButtonStatus = true;
+  }
+}
+
+class AdvertisingCallback: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    if (advertisedDevice.getName() == TT_BLE_NAME) {
+      advertisedDevice.getScan()->stop();
+      pRemoteAddress = new BLEAddress(advertisedDevice.getAddress());
+      needsConnect = true;
+      Serial.println(" ---> Found remote device, connecting...");
+    }
+  }
+};
 
 void setup() {
   hspi->begin();
@@ -62,18 +145,46 @@ void setup() {
 
   display.display();
 
+  BLEDevice::init("");
+  BLEScan *pScanner = BLEDevice::getScan();
+  pScanner->setAdvertisedDeviceCallbacks(new AdvertisingCallback());
+  pScanner->setActiveScan(true);
+  pScanner->start(30, false);
 }
 
 void loop() {
   Serial.println("Hello!");
-  
   // set color to red
-  pixels.fill(0xFF0000);
+  pixels.fill(0xFF00FF);
+  pixels.show();
+  delay(500); // wait half a second
+  pixels.fill(0x00FFFF);
+  pixels.show();
+  delay(500); // wait half a second
+  pixels.fill(0xFF00FF);
+  pixels.show();
+  delay(500); // wait half a second
+  pixels.fill(0xFFFF00);
   pixels.show();
   delay(500); // wait half a second
 
-  // turn off
-  pixels.fill(0x00FF00);
-  pixels.show();
-  delay(500); // wait half a second
+
+  
+  if (needsConnect) {
+    if (connectToRemoteDevice(*pRemoteAddress)) {
+      Serial.println(" ---> Connected to remote");
+      buttonStatusCharacteristic->getDescriptor(BLEUUID("2902"))->writeValue((uint8_t *)notificationOn, 2, true);
+      isConnected = true;
+    } else {
+      Serial.println(" ---> Failed to connect to remote");
+    }
+    needsConnect = false;
+  }
+
+  if (newButtonStatus) {
+    newButtonStatus = false;
+    pixels.fill(0x0000FF);
+    pixels.show();
+    delay(1500); // wait half a second
+  }
 }
